534205aaa6bb40729ef4f9aebbcaa2eb7ecbb9cf
loongluangkhot <loongluangkhot@gmail.com>
2024-04-13 15:14:12 +0800

    Configure sequelize to use ssl to overcome error when running locally

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,26 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
+import { SequelizeConfig } from 'sequelize-typescript/lib/types/SequelizeConfig';
 
-
-export const sequelize = new Sequelize({
+let sequelizeConfig: SequelizeConfig = {
   'username': config.username,
   'password': config.password,
   'database': config.database,
   'host': config.host,
-
   'dialect': config.dialect,
   'storage': ':memory:',
-});
+};
+
+if(process.env.NODE_ENV === "local") {
+  sequelizeConfig = {
+    ...sequelizeConfig,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      }
+    },
+  };
+}
+
+export const sequelize = new Sequelize(sequelizeConfig);
